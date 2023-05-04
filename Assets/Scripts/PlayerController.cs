8a73328ae32d20adf379339c8c6b8d95bc0a1b13
SAJ33L <sajeel.khan01@gmail.com>
2023-05-04 19:59:36 +0500

    completed the tutorial

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,25 +2,48 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 5.0f;
-    public float turnspeed;
+    //public variable for speed
+    //public float speed = 5.0f;
+    //public variable for turning speed
+    //public float turnSpeed;
+    //using a public variable to get horizontal-axis
+    //public float horizontalInput;
+    //using a public variable to get vertical-axis
+    //public float forwardInput;
 
-    // Start is called before the first frame update
+    //making all the inputs private
+    private float speed = 20.0f;
+    private float turnSpeed = 5.0f;
+    private float horizontalInput;
+    private float forwardInput;
+
+    //Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    //Update is called once per frame
     void Update()
     {
-        // We'll move the vehicle forward
-
-        // using simple transform.translate(x, y, z)
+        //We'll move the vehicle forward
+        //using simple transform.translate(x, y, z)
         //transform.Translate(0, 0, 1);
+        //now using vector3.forward to move the vehicle at the change in frame persecond
+        //transform.Translate(Vector3.forward * Time.deltaTime *speed);
+        //now using vector3.forward to move the vehicle right or left
+        //transform.Translate(Vector3.right * Time.deltaTime * turnSpeed);
 
-        // now using vector3.forward
-        transform.Translate(Vector3.forward * Time.deltaTime *speed);
-        transform.Translate(Vector3.right * Time.deltaTime * turnspeed);
+        //getting the horizontal axis from predefined horizontal keys in unity
+        horizontalInput = Input.GetAxis("Horizontal");
+        //now using vector3.right to move the vehicle right or left
+        //transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
+        //using transform.rotated instead
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput);
+        //getting the horizontal axis from predefined vertical keys in unity
+        forwardInput = Input.GetAxis("Vertical");
+        //now using vector3.forward to move the vehicle forward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+
     }
 }
